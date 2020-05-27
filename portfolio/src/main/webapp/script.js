302c7c9732fbdd0903caa5a697e4f8efdec2c132
Bria <briafassler@gmail.com>
2020-05-27 16:13:19 -0400

    Portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I like mint green', 'I have 2 dogs', 'I go to Howard University', 'I live in Maryland'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
