e2403ca78180b604faa5b966833d9625e009b38c
Bria <briafassler@gmail.com>
2020-06-02 19:53:13 +0000

    Adds and displays list of items upon button being pressed.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
+
+
 public class DataServlet extends HttpServlet {
+  
+  private ArrayList<String> list;
+
+  public DataServlet(){ 
+    list = new ArrayList<String>();
+      list.add("Fruit");
+      list.add("Soap");
+      list.add("Cereal");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("Hello Bria!");
+     Gson gson = new Gson();
+    String json = gson.toJson(list);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
