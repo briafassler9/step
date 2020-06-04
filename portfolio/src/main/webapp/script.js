ac0e43843ef8646bac095c7e96240e3f61b021d1
Bria <briafassler@gmail.com>
2020-06-04 20:03:32 +0000

    Loads the comments and creates delete button.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,12 +26,41 @@ function randomDogImage(){
   imageRemover.appendChild(imageElement);
 }
 
-/*A function that fetches the Data Servlet*/
-function getGreeting() {
-  fetch('/data').then(response => response.text()).then((response) => {
-    document.getElementById('quote-container').innerText = response;
+function loadComments() {
+  fetch('/data').then(response => response.json()).then(data => {
+    const commentListElement = document.getElementById('task-list');
+    data.forEach((task) => {
+    commentListElement.appendChild(createCommentElement(task));
+    })
   });
 }
 
+/** Creates an element that represents a task, including its delete button. */
+function createCommentElement(task) {
+  const taskElement = document.createElement('li');
+  taskElement.className = 'task';
 
+  const titleElement = document.createElement('span');
+  titleElement.innerText = task;
+
+  const deleteButtonElement = document.createElement('button');
+  deleteButtonElement.innerText = 'Delete';
+  deleteButtonElement.addEventListener('click', () => {
+  deleteTask(task);
+
+  // Remove the task from the DOM.
+  taskElement.remove();
+  });
+
+  taskElement.appendChild(titleElement);
+  taskElement.appendChild(deleteButtonElement);
+  return taskElement;
+}
+
+/** Tells the server to delete the task. */
+function deleteTask(task) {
+  const params = new URLSearchParams();
+  params.append('id', task.id);
+  fetch('/delete-task', {method: 'POST', body: params});
+}
 
